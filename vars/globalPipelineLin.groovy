5f01d1ef90607be5430cf872192e9a0e834b8950
GrantLee289 <grantlee289@gmail.com>
2020-04-05 18:29:42 +0100

    added empty url

--- vars/globalPipelineLin.groovy
+++ vars/globalPipelineLin.groovy
@@ -100,7 +100,7 @@ def call(Map pipelineParameters) {
 
             stage('Push image to Docker Hub') {
                 steps {
-                    withDockerRegistry([credentialsId: 'dockerCreds']) {
+                    withDockerRegistry([credentialsId: 'dockerCreds', url: ""]) {
                         sh "sudo docker push ${DOCKERUSER}/${PROJECT_NAME}"
                     }
 //                    withCredentials([usernamePassword(credentialsId: 'dockerCreds', usernameVariable: 'user', passwordVariable: 'password')]) {
